2d8dbad49399cf5e2568b35f4308da426c185ab2
Netanel Kaufman <kaufman.netanel@gmail.com>
2013-03-14 21:39:04 +0200

    Add the adapter and connect to the menu

---
+++ TodoListManager/src/il/ac/huji/todolist/Todo.java
@@ -0,0 +1,15 @@
+package il.ac.huji.todolist;
+
+public class Todo {
+	
+	public Todo(String task) {
+		this.task = task;
+	}
+	
+	public String task;
+	
+	public String toString() {
+		return task;
+	}
+	
+}
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,17 +1,37 @@
 package il.ac.huji.todolist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class TodoListManagerActivity extends Activity {
 
+	private ArrayAdapter<Todo> adapter;
+	private List<Todo> todos;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list_manager);
+        
+        todos = new ArrayList<Todo>();
+        ListView todoListView = 
+        		(ListView)findViewById(R.id.lstTodoItems);
+        adapter =   new ArrayAdapter<Todo>(
+        			this,
+        			android.R.layout.simple_list_item_1,
+        			todos
+        			);
+        todoListView.setAdapter(adapter);
+        
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +39,22 @@ public class TodoListManagerActivity extends Activity {
         getMenuInflater().inflate(R.menu.todo_list_manager, menu);
         return true;
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.menuItemAdd:
+        	EditText newTask = (EditText)findViewById(R.id.edtNewItem);
+    		adapter.add(new Todo(newTask.getText().toString()));
+    		break;
+    	case R.id.menuItemDelete:
+    		ListView todosListView = (ListView)findViewById(R.id.lstTodoItems);
+    		Todo todo = (Todo)todosListView.getSelectedItem();
+    		adapter.remove(todo);
+    		break;
+    	}
+    	return true;
+    }
+
     
 }
